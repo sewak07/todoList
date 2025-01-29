904b29b36811e7192262c6537122aa2771e2b8c0
sewak07 <160036481+sewak07@users.noreply.github.com>
2025-01-29 21:25:25 +0545

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,35 @@
+let container = document.querySelector(".container");
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+
+
+
+// Add event listener to the button
+btn.addEventListener("click", () => {
+   
+
+   // Create a new input element
+   let newInput = document.createElement("input");
+   newInput.value= input.value;
+
+   //create a button 
+   let newBtn=document.createElement("button");
+   newBtn.innerHTML = '<i class="fas fa-trash-alt"></i>'; 
+   
+   container.appendChild(newInput);
+   container.appendChild(newBtn);
+
+   // Clear the value of the input field
+   input.value = "";
+  
+   
+   
+   newBtn.addEventListener("click",()=>{
+      container.removeChild(newBtn);
+      container.removeChild(newInput);
+   });
+});
+
+
+
+
