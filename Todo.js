9e540fee8ce295579e7737aff0c61dbe73a6d02c
Sewak Dhakal <160036481+sewak07@users.noreply.github.com>
2025-02-12 23:11:19 +0545

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -2,7 +2,8 @@ let container = document.querySelector(".container");
 let input = document.querySelector("input");
 let btn = document.querySelector("button");
 
-
+let ul=document.createElement("ul");
+container.appendChild(ul);
 
 // Add event listener to the button
 btn.addEventListener("click", () => {
@@ -10,13 +11,13 @@ btn.addEventListener("click", () => {
 
    // Create a new input element
    let newInput = document.createElement("input");
-   newInput.value= input.value;
+   newInput.innerText= input.value;
 
    //create a button 
    let newBtn=document.createElement("button");
    newBtn.innerHTML = '<i class="fas fa-trash-alt"></i>'; 
    
-   container.appendChild(newInput);
+   ul.appendChild(newInput);
    container.appendChild(newBtn);
 
    // Clear the value of the input field
